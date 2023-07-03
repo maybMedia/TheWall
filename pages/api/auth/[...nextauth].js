d68ddcc7ec50da564b3f9b7233ba41863d19aeb2
maybMedia <braithmay@yahoo.com.au>
2023-07-04 00:16:09 +1000

    Basic navbar with server side auth logic.

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,12 +3,14 @@ import GoogleProvider from "next-auth/providers/google"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import prisma from '../../../prisma/client'
 
-export default NextAuth({
-    adapter: PrismaAdapter(prisma),
-    providers: [
-      GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      }),
-    ],
-  });
+export const authOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+}
+
+export default NextAuth(authOptions)
